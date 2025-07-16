e0763cda011b65052a5df853e3bd6468774b99ed
RashmiJanve0801 <146350029+RashmiJanve0801@users.noreply.github.com>
2025-07-16 11:11:25 +0530

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from config.constant import MODEL
+
+load_dotenv()
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+def get_model_client():
+    model_client = OpenAIChatCompletionClient(model=MODEL)
+    return model_client
