66ca3739e709196fa776587f4449acec7ab1bcd2
RashmiJanve0801 <146350029+RashmiJanve0801@users.noreply.github.com>
2025-07-16 11:26:42 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,8 @@ from config.constant import MODEL
 
 load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def get_model_client():
-    model_client = OpenAIChatCompletionClient(model=MODEL)
+    model_client = OpenAIChatCompletionClient(model=MODEL, api_key=API_KEY)
     return model_client
